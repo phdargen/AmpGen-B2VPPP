696140eb417dcad1a9905df3f03cdd733c8076b4
phdargen <p.dargent@cern.ch>
2023-02-02 15:59:14 +0100

    updated scans

--- build/bin/submit_scans.sh
+++ build/bin/submit_scans.sh
@@ -9,20 +9,20 @@
 #condor_submit o=scan_psiK_P.txt d=scan_psiK_P n=120 submitScan.sub
 #condor_submit o=scan_psiK_V.txt d=scan_psiK_V n=120 submitScan.sub
 #condor_submit o=scan_psiK_A.txt d=scan_psiK_A n=120 submitScan.sub
-condor_submit o=scan_psiK_T.txt d=scan_psiK_T n=120 submitScan.sub
-condor_submit o=scan_psiK_PT.txt d=scan_psiK_PT n=120 submitScan.sub
+#condor_submit o=scan_psiK_T.txt d=scan_psiK_T n=120 submitScan.sub
+#condor_submit o=scan_psiK_PT.txt d=scan_psiK_PT n=120 submitScan.sub
 
-#condor_submit o=scan_psipipi_S.txt d=scan_psipipi_S n=120 submitScan.sub
-condor_submit o=scan_psipipi_A.txt d=scan_psipipi_A n=120 submitScan.sub
-condor_submit o=scan_psipipi_V.txt d=scan_psipipi_V n=120 submitScan.sub
-condor_submit o=scan_psipipi_T.txt d=scan_psipipi_T n=120 submitScan.sub
-condor_submit o=scan_psipipi_PT.txt d=scan_psipipi_PT n=120 submitScan.sub
+#condor_submit o=scan_psipipi_S.txt d=scan_psipipi_S2 n=120 submitScan.sub
+#condor_submit o=scan_psipipi_A.txt d=scan_psipipi_A2 n=120 submitScan.sub
+#condor_submit o=scan_psipipi_V.txt d=scan_psipipi_V2 n=120 submitScan.sub
+#condor_submit o=scan_psipipi_T.txt d=scan_psipipi_T2 n=99 submitScan.sub
+#condor_submit o=scan_psipipi_PT.txt d=scan_psipipi_PT2 n=99 submitScan.sub
 
-condor_submit o=scan_psipi_P.txt d=scan_psipi_P n=120 submitScan.sub
+#condor_submit o=scan_psipi_P.txt d=scan_psipi_P n=120 submitScan.sub
 #condor_submit o=scan_psipi_A.txt d=scan_psipi_A n=120 submitScan.sub
-condor_submit o=scan_psipi_V.txt d=scan_psipi_V n=120 submitScan.sub
-condor_submit o=scan_psipi_T.txt d=scan_psipi_T n=120 submitScan.sub
-condor_submit o=scan_psipi_PT.txt d=scan_psipi_PT n=120 submitScan.sub
+#condor_submit o=scan_psipi_V.txt d=scan_psipi_V n=120 submitScan.sub
+#condor_submit o=scan_psipi_T.txt d=scan_psipi_T n=120 submitScan.sub
+#condor_submit o=scan_psipi_PT.txt d=scan_psipi_PT n=120 submitScan.sub
 
 condor_submit o=scan_Kpipi_P.txt d=scan_Kpipi_P n=99 submitScan.sub
 condor_submit o=scan_Kpipi_A.txt d=scan_Kpipi_A n=99 submitScan.sub
@@ -31,3 +31,24 @@ condor_submit o=scan_Kpipi_T.txt d=scan_Kpipi_T n=99 submitScan.sub
 condor_submit o=scan_Kpipi_PT.txt d=scan_Kpipi_PT n=99 submitScan.sub
 
  
+#condor_submit o=scan_psiKpi2_S.txt d=scan_psiKpi2_S n=120 submitScan.sub                                                                                                                                  
+#condor_submit o=scan_psiKpi2_A.txt d=scan_psiKpi2_A n=120 submitScan.sub                                                                                                                                  
+#condor_submit o=scan_psiKpi2_V.txt d=scan_psiKpi2_V n=120 submitScan.sub                                                                                                                                  
+#condor_submit o=scan_psiKpi2_T.txt d=scan_psiKpi2_T n=99 submitScan.sub                                                                                                                                  
+#condor_submit o=scan_psiKpi2_PT.txt d=scan_psiKpi2_PT n=99 submitScan.sub                                                                                                                                 
+
+#condor_submit o=scan_psiK2_P.txt d=scan_psiK2_P n=120 submitScan.sub                                                                                                                                      
+#condor_submit o=scan_psiK2_V.txt d=scan_psiK2_V n=120 submitScan.sub                                                                                                                                      
+#condor_submit o=scan_psiK2_A.txt d=scan_psiK2_A n=120 submitScan.sub                                                                                                                                      
+#condor_submit o=scan_psiK2_T.txt d=scan_psiK2_T n=99 submitScan.sub                                                                                                                                       
+#condor_submit o=scan_psiK2_PT.txt d=scan_psiK2_PT n=99 submitScan.sub                                                                                                                                       
+#condor_submit o=scan_psipipi2_S.txt d=scan_psipipi2_S n=120 submitScan.sub                                                                                                                                
+#condor_submit o=scan_psipipi2_A.txt d=scan_psipipi2_A n=120 submitScan.sub                                                                                                                                
+#condor_submit o=scan_psipipi2_V.txt d=scan_psipipi2_V n=120 submitScan.sub                                                                                                                                
+#condor_submit o=scan_psipipi2_T.txt d=scan_psipipi2_T n=99 submitScan.sub                                                                                                                                 
+#condor_submit o=scan_psipipi2_PT.txt d=scan_psipipi2_PT n=99 submitScan.sub                                                                                                                                 
+#condor_submit o=scan_psipi2_P.txt d=scan_psipi2_P n=120 submitScan.sub                                                                                                                                    
+#condor_submit o=scan_psipi2_A.txt d=scan_psipi2_A n=120 submitScan.sub                                                                                                                                    
+#condor_submit o=scan_psipi2_V.txt d=scan_psipi2_V n=120 submitScan.sub                                                                                                                                    
+#condor_submit o=scan_psipi2_T.txt d=scan_psipi2_T n=99 submitScan.sub                                                                                                                                     
+#condor_submit o=scan_psipi2_PT.txt d=scan_psipi2_PT n=99 submitScan.sub  
