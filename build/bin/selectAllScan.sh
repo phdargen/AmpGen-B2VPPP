83256e2f1ab8ed7035e6c778b6e386603626d9ff
phdargen <p.dargent@cern.ch>
2023-02-06 21:10:30 +0100

    prepare new model selection

--- build/bin/selectAllScan.sh
+++ build/bin/selectAllScan.sh
@@ -15,11 +15,11 @@
 #./selectModel.exe scan_psiK_PT.txt --ResultsFile=scan_psiK_PT/result.root --ResultsBaseFile=scan_psiK_P/result.root --nScan=120
 
 # check nBins in scanParam                                                                                                                                                                                 
-./selectModel.exe scan_psiK_P.txt --ResultsFile=scan_psiK_P2/result.root --ResultsBaseFile=scan_psiK_P2/result.root --nScan=120                                                                       
-./selectModel.exe scan_psiK_V.txt --ResultsFile=scan_psiK_V2/result.root --ResultsBaseFile=scan_psiK_P2/result.root --nScan=120                                                                      
-./selectModel.exe scan_psiK_A.txt --ResultsFile=scan_psiK_A2/result.root --ResultsBaseFile=scan_psiK_P2/result.root --nScan=120                                                                    
-./selectModel.exe scan_psiK_T.txt --ResultsFile=scan_psiK_T2/result.root --ResultsBaseFile=scan_psiK_P2/result.root --nScan=120                                                                      
-./selectModel.exe scan_psiK_PT.txt --ResultsFile=scan_psiK_PT2/result.root --ResultsBaseFile=scan_psiK_P2/result.root --nScan=120 
+#./selectModel.exe scan_psiK_P.txt --ResultsFile=scan_psiK_P2/result.root --ResultsBaseFile=scan_psiK_P2/result.root --nScan=120                                                                       
+#./selectModel.exe scan_psiK_V.txt --ResultsFile=scan_psiK_V2/result.root --ResultsBaseFile=scan_psiK_P2/result.root --nScan=120                                                                      
+#./selectModel.exe scan_psiK_A.txt --ResultsFile=scan_psiK_A2/result.root --ResultsBaseFile=scan_psiK_P2/result.root --nScan=120                                                                    
+#./selectModel.exe scan_psiK_T.txt --ResultsFile=scan_psiK_T2/result.root --ResultsBaseFile=scan_psiK_P2/result.root --nScan=120                                                                      
+#./selectModel.exe scan_psiK_PT.txt --ResultsFile=scan_psiK_PT2/result.root --ResultsBaseFile=scan_psiK_P2/result.root --nScan=120 
 
 # check nBins in scanParam                                                                                                                                                                                 
 #./selectModel.exe scan_psipipi_S.txt --ResultsFile=scan_psipipi_S/result.root --ResultsBaseFile=scan_psipipi_S/result.root --nScan=150
@@ -28,17 +28,29 @@
 #./selectModel.exe scan_psipipi_T.txt --ResultsFile=scan_psipipi_T/result.root --ResultsBaseFile=scan_psipipi_S/result.root --nScan=120
 #./selectModel.exe scan_psipipi_PT.txt --ResultsFile=scan_psipipi_PT/result.root --ResultsBaseFile=scan_psipipi_S/result.root --nScan=120
 
-./selectModel.exe scan_psipipi_S.txt --ResultsFile=scan_psipipi_S2/result.root --ResultsBaseFile=scan_psipipi_S2/result.root --nScan=120
-./selectModel.exe scan_psipipi_A.txt --ResultsFile=scan_psipipi_A2/result.root --ResultsBaseFile=scan_psipipi_S2/result.root --nScan=120
-./selectModel.exe scan_psipipi_V.txt --ResultsFile=scan_psipipi_V2/result.root --ResultsBaseFile=scan_psipipi_S2/result.root --nScan=120
-./selectModel.exe scan_psipipi_T.txt --ResultsFile=scan_psipipi_T2/result.root --ResultsBaseFile=scan_psipipi_S2/result.root --nScan=120
-./selectModel.exe scan_psipipi_PT.txt --ResultsFile=scan_psipipi_PT2/result.root --ResultsBaseFile=scan_psipipi_S2/result.root --nScan=120
+#./selectModel.exe scan_psipipi_S.txt --ResultsFile=scan_psipipi_S2/result.root --ResultsBaseFile=scan_psipipi_S2/result.root --nScan=120
+#./selectModel.exe scan_psipipi_A.txt --ResultsFile=scan_psipipi_A2/result.root --ResultsBaseFile=scan_psipipi_S2/result.root --nScan=120
+#./selectModel.exe scan_psipipi_V.txt --ResultsFile=scan_psipipi_V2/result.root --ResultsBaseFile=scan_psipipi_S2/result.root --nScan=120
+#./selectModel.exe scan_psipipi_T.txt --ResultsFile=scan_psipipi_T2/result.root --ResultsBaseFile=scan_psipipi_S2/result.root --nScan=120
+#./selectModel.exe scan_psipipi_PT.txt --ResultsFile=scan_psipipi_PT2/result.root --ResultsBaseFile=scan_psipipi_S2/result.root --nScan=120
+
+#./selectModel.exe scan_psipipi_S.txt --ResultsFile=scan_psipipi_S3/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
+#./selectModel.exe scan_psipipi_A.txt --ResultsFile=scan_psipipi_A3/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
+#./selectModel.exe scan_psipipi_V.txt --ResultsFile=scan_psipipi_V3/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
+#./selectModel.exe scan_psipipi_T.txt --ResultsFile=scan_psipipi_T3/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
+#./selectModel.exe scan_psipipi_PT.txt --ResultsFile=scan_psipipi_PT3/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
 
 #./selectModel.exe scan_psipi_P.txt --ResultsFile=scan_psipi_P/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
 #./selectModel.exe scan_psipi_A.txt --ResultsFile=scan_psipi_A/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
 #./selectModel.exe scan_psipi_V.txt --ResultsFile=scan_psipi_V/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
 #./selectModel.exe scan_psipi_T.txt --ResultsFile=scan_psipi_T/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
 #./selectModel.exe scan_psipi_PT.txt --ResultsFile=scan_psipi_PT/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
+
+#./selectModel.exe scan_psipi_P.txt --ResultsFile=scan_psipi_P2/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120                                                              
+#./selectModel.exe scan_psipi_A.txt --ResultsFile=scan_psipi_A2/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120                                                                   
+./selectModel.exe scan_psipi_V.txt --ResultsFile=scan_psipi_V2/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120                                                                   
+./selectModel.exe scan_psipi_T.txt --ResultsFile=scan_psipi_T2/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120                                                                      
+./selectModel.exe scan_psipi_PT.txt --ResultsFile=scan_psipi_PT2/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120                                                                        
 
 #./selectModel.exe scan_Kpipi_P.txt --ResultsFile=scan_Kpipi_P/result.root --ResultsBaseFile=scan_Kpipi_P/result.root --nScan=99
 #./selectModel.exe scan_Kpipi_V.txt --ResultsFile=scan_Kpipi_V/result.root --ResultsBaseFile=scan_Kpipi_P/result.root --nScan=99
@@ -47,26 +59,50 @@
 #./selectModel.exe scan_Kpipi_PT.txt --ResultsFile=scan_Kpipi_PT/result.root --ResultsBaseFile=scan_Kpipi_P/result.root --nScan=99
 
                                                                                                                                                                                  
-./selectModel.exe scan_psiKpi2_S.txt --ResultsFile=scan_psiKpi2_S/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
-./selectModel.exe scan_psiKpi2_V.txt --ResultsFile=scan_psiKpi2_V/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
-./selectModel.exe scan_psiKpi2_A.txt --ResultsFile=scan_psiKpi2_A/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
-./selectModel.exe scan_psiKpi2_T.txt --ResultsFile=scan_psiKpi2_T/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
-./selectModel.exe scan_psiKpi2_PT.txt --ResultsFile=scan_psiKpi2_PT/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
+#./selectModel.exe scan_psiKpi2_S.txt --ResultsFile=scan_psiKpi2_S/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
+#./selectModel.exe scan_psiKpi2_V.txt --ResultsFile=scan_psiKpi2_V/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
+#./selectModel.exe scan_psiKpi2_A.txt --ResultsFile=scan_psiKpi2_A/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
+#./selectModel.exe scan_psiKpi2_T.txt --ResultsFile=scan_psiKpi2_T/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
+#./selectModel.exe scan_psiKpi2_PT.txt --ResultsFile=scan_psiKpi2_PT/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=120
                                                                                                                                                                                  
-./selectModel.exe scan_psiK2_P.txt --ResultsFile=scan_psiK2_P/result.root --ResultsBaseFile=scan_psiK_P/result.root --nScan=120
-./selectModel.exe scan_psiK2_V.txt --ResultsFile=scan_psiK2_V/result.root --ResultsBaseFile=scan_psiK_P/result.root --nScan=120
-./selectModel.exe scan_psiK2_A.txt --ResultsFile=scan_psiK2_A/result.root --ResultsBaseFile=scan_psiK_P/result.root --nScan=120
-./selectModel.exe scan_psiK2_T.txt --ResultsFile=scan_psiK2_T/result.root --ResultsBaseFile=scan_psiK_P/result.root --nScan=120
-./selectModel.exe scan_psiK2_PT.txt --ResultsFile=scan_psiK2_PT/result.root --ResultsBaseFile=scan_psiK_P/result.root --nScan=120
+#./selectModel.exe scan_psiK2_P.txt --ResultsFile=scan_psiK2_P/result.root --ResultsBaseFile=scan_psiK_P/result.root --nScan=120
+#./selectModel.exe scan_psiK2_V.txt --ResultsFile=scan_psiK2_V/result.root --ResultsBaseFile=scan_psiK_P/result.root --nScan=120
+#./selectModel.exe scan_psiK2_A.txt --ResultsFile=scan_psiK2_A/result.root --ResultsBaseFile=scan_psiK_P/result.root --nScan=120
+#./selectModel.exe scan_psiK2_T.txt --ResultsFile=scan_psiK2_T/result.root --ResultsBaseFile=scan_psiK_P/result.root --nScan=120
+#./selectModel.exe scan_psiK2_PT.txt --ResultsFile=scan_psiK2_PT/result.root --ResultsBaseFile=scan_psiK_P/result.root --nScan=120
 
-./selectModel.exe scan_psipipi2_S.txt --ResultsFile=scan_psipipi2_S/result.root --ResultsBaseFile=scan_psipipi2_S/result.root --nScan=120
-./selectModel.exe scan_psipipi2_A.txt --ResultsFile=scan_psipipi2_A/result.root --ResultsBaseFile=scan_psipipi2_S/result.root --nScan=120
-./selectModel.exe scan_psipipi2_V.txt --ResultsFile=scan_psipipi2_V/result.root --ResultsBaseFile=scan_psipipi2_S/result.root --nScan=120
-./selectModel.exe scan_psipipi2_T.txt --ResultsFile=scan_psipipi2_T/result.root --ResultsBaseFile=scan_psipipi2_S/result.root --nScan=120
-./selectModel.exe scan_psipipi2_PT.txt --ResultsFile=scan_psipipi2_PT/result.root --ResultsBaseFile=scan_psipipi2_S/result.root --nScan=120
+#./selectModel.exe scan_psipipi2_S.txt --ResultsFile=scan_psipipi2_S/result.root --ResultsBaseFile=scan_psipipi2_S/result.root --nScan=120
+#./selectModel.exe scan_psipipi2_A.txt --ResultsFile=scan_psipipi2_A/result.root --ResultsBaseFile=scan_psipipi2_S/result.root --nScan=120
+#./selectModel.exe scan_psipipi2_V.txt --ResultsFile=scan_psipipi2_V/result.root --ResultsBaseFile=scan_psipipi2_S/result.root --nScan=120
+#./selectModel.exe scan_psipipi2_T.txt --ResultsFile=scan_psipipi2_T/result.root --ResultsBaseFile=scan_psipipi2_S/result.root --nScan=120
+#./selectModel.exe scan_psipipi2_PT.txt --ResultsFile=scan_psipipi2_PT/result.root --ResultsBaseFile=scan_psipipi2_S/result.root --nScan=120
 
-./selectModel.exe scan_psipi2_P.txt --ResultsFile=scan_psipi2_P/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
-./selectModel.exe scan_psipi2_A.txt --ResultsFile=scan_psipi2_A/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
-./selectModel.exe scan_psipi2_V.txt --ResultsFile=scan_psipi2_V/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
-./selectModel.exe scan_psipi2_T.txt --ResultsFile=scan_psipi2_T/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
-./selectModel.exe scan_psipi2_PT.txt --ResultsFile=scan_psipi2_PT/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
+#./selectModel.exe scan_psipi2_P.txt --ResultsFile=scan_psipi2_P/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
+#./selectModel.exe scan_psipi2_A.txt --ResultsFile=scan_psipi2_A/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
+#./selectModel.exe scan_psipi2_V.txt --ResultsFile=scan_psipi2_V/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
+#./selectModel.exe scan_psipi2_T.txt --ResultsFile=scan_psipi2_T/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
+#./selectModel.exe scan_psipi2_PT.txt --ResultsFile=scan_psipi2_PT/result.root --ResultsBaseFile=scan_psipi_P/result.root --nScan=120
+
+./selectModel.exe scan_X2Z_SP.txt --ResultsFile=scan_X2Z_SP/result.root --ResultsBaseFile=scan_X2Z_SP/result.root --nScan=90
+./selectModel.exe scan_X2Z_SA.txt --ResultsFile=scan_X2Z_SA/result.root --ResultsBaseFile=scan_X2Z_SP/result.root --nScan=90
+./selectModel.exe scan_X2Z_AA.txt --ResultsFile=scan_X2Z_AA/result.root --ResultsBaseFile=scan_X2Z_SP/result.root --nScan=90
+./selectModel.exe scan_X2Z_AV.txt --ResultsFile=scan_X2Z_AV/result.root --ResultsBaseFile=scan_X2Z_SP/result.root --nScan=90
+./selectModel.exe scan_X2Z_VP.txt --ResultsFile=scan_X2Z_VP/result.root --ResultsBaseFile=scan_X2Z_SP/result.root --nScan=90
+./selectModel.exe scan_X2Z_VV.txt --ResultsFile=scan_X2Z_VV/result.root --ResultsBaseFile=scan_X2Z_SP/result.root --nScan=90
+./selectModel.exe scan_X2Z_VA.txt --ResultsFile=scan_X2Z_VA/result.root --ResultsBaseFile=scan_X2Z_SP/result.root --nScan=90
+                                                                         
+./selectModel.exe scan_Xs2Z_SP.txt --ResultsFile=scan_Xs2Z_SP/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Z_SA.txt --ResultsFile=scan_Xs2Z_SA/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Z_AA.txt --ResultsFile=scan_Xs2Z_AA/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Z_AV.txt --ResultsFile=scan_Xs2Z_AV/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Z_VP.txt --ResultsFile=scan_Xs2Z_VP/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Z_VV.txt --ResultsFile=scan_Xs2Z_VV/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Z_VA.txt --ResultsFile=scan_Xs2Z_VA/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+
+./selectModel.exe scan_Xs2Zs_SP.txt --ResultsFile=scan_Xs2Zs_SP/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Zs_SA.txt --ResultsFile=scan_Xs2Zs_SA/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Zs_AA.txt --ResultsFile=scan_Xs2Zs_AA/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Zs_AV.txt --ResultsFile=scan_Xs2Zs_AV/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Zs_VP.txt --ResultsFile=scan_Xs2Zs_VP/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Zs_VV.txt --ResultsFile=scan_Xs2Zs_VV/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
+./selectModel.exe scan_Xs2Zs_VA.txt --ResultsFile=scan_Xs2Zs_VA/result.root --ResultsBaseFile=scan_psiKpi/result.root --nScan=90
--- build/bin/submit_scans.sh
+++ build/bin/submit_scans.sh
@@ -12,23 +12,23 @@
 #condor_submit o=scan_psiK_T.txt d=scan_psiK_T n=120 submitScan.sub
 #condor_submit o=scan_psiK_PT.txt d=scan_psiK_PT n=120 submitScan.sub
 
-condor_submit o=scan_psiK_P.txt d=scan_psiK_P2 n=120 submitScan.sub                                                                                                                                        
-condor_submit o=scan_psiK_V.txt d=scan_psiK_V2 n=120 submitScan.sub                                                                                                                                        
-condor_submit o=scan_psiK_A.txt d=scan_psiK_A2 n=120 submitScan.sub                                                                                                                                        
-condor_submit o=scan_psiK_T.txt d=scan_psiK_T2 n=120 submitScan.sub                                                                                                                                        
-condor_submit o=scan_psiK_PT.txt d=scan_psiK_PT2 n=120 submitScan.sub  
+#condor_submit o=scan_psiK_P.txt d=scan_psiK_P2 n=120 submitScan.sub                                                                                                                                       
+#condor_submit o=scan_psiK_V.txt d=scan_psiK_V2 n=120 submitScan.sub                                                                                                                                       
+#condor_submit o=scan_psiK_A.txt d=scan_psiK_A2 n=120 submitScan.sub                                                                                                                                      
+#condor_submit o=scan_psiK_T.txt d=scan_psiK_T2 n=120 submitScan.sub                                                                                                                                      
+#condor_submit o=scan_psiK_PT.txt d=scan_psiK_PT2 n=120 submitScan.sub  
 
-#condor_submit o=scan_psipipi_S.txt d=scan_psipipi_S2 n=120 submitScan.sub
-#condor_submit o=scan_psipipi_A.txt d=scan_psipipi_A2 n=120 submitScan.sub
-#condor_submit o=scan_psipipi_V.txt d=scan_psipipi_V2 n=120 submitScan.sub
-#condor_submit o=scan_psipipi_T.txt d=scan_psipipi_T2 n=120 submitScan.sub
-#condor_submit o=scan_psipipi_PT.txt d=scan_psipipi_PT2 n=120 submitScan.sub
+#condor_submit o=scan_psipipi_S.txt d=scan_psipipi_S3 n=120 submitScan.sub
+#condor_submit o=scan_psipipi_A.txt d=scan_psipipi_A3 n=120 submitScan.sub
+#condor_submit o=scan_psipipi_V.txt d=scan_psipipi_V3 n=120 submitScan.sub
+#condor_submit o=scan_psipipi_T.txt d=scan_psipipi_T3 n=120 submitScan.sub
+#condor_submit o=scan_psipipi_PT.txt d=scan_psipipi_PT3 n=120 submitScan.sub
 
-#condor_submit o=scan_psipi_P.txt d=scan_psipi_P n=120 submitScan.sub
-#condor_submit o=scan_psipi_A.txt d=scan_psipi_A n=120 submitScan.sub
-#condor_submit o=scan_psipi_V.txt d=scan_psipi_V n=120 submitScan.sub
-#condor_submit o=scan_psipi_T.txt d=scan_psipi_T n=120 submitScan.sub
-#condor_submit o=scan_psipi_PT.txt d=scan_psipi_PT n=120 submitScan.sub
+#condor_submit o=scan_psipi_P.txt d=scan_psipi_P2 n=120 submitScan.sub
+#condor_submit o=scan_psipi_A.txt d=scan_psipi_A2 n=120 submitScan.sub
+#condor_submit o=scan_psipi_V.txt d=scan_psipi_V2 n=120 submitScan.sub
+#condor_submit o=scan_psipi_T.txt d=scan_psipi_T2 n=120 submitScan.sub
+#condor_submit o=scan_psipi_PT.txt d=scan_psipi_PT2 n=120 submitScan.sub
 
 #condor_submit o=scan_Kpipi_P.txt d=scan_Kpipi_P n=99 submitScan.sub
 #condor_submit o=scan_Kpipi_A.txt d=scan_Kpipi_A n=99 submitScan.sub
@@ -57,3 +57,29 @@ condor_submit o=scan_psiK_PT.txt d=scan_psiK_PT2 n=120 submitScan.sub
 #condor_submit o=scan_psipi2_V.txt d=scan_psipi2_V n=120 submitScan.sub                                                                                                                                    
 #condor_submit o=scan_psipi2_T.txt d=scan_psipi2_T n=120 submitScan.sub                                                                                                                                    
 #condor_submit o=scan_psipi2_PT.txt d=scan_psipi2_PT n=120 submitScan.sub  
+
+
+#condor_submit o=scan_X2Z_SP.txt d=scan_X2Z_SP n=90 submitScan.sub                                                                                                                                    
+#condor_submit o=scan_X2Z_SA.txt d=scan_X2Z_SA n=90 submitScan.sub
+#condor_submit o=scan_X2Z_VP.txt d=scan_X2Z_VP n=90 submitScan.sub
+#condor_submit o=scan_X2Z_VV.txt d=scan_X2Z_VV n=90 submitScan.sub
+#condor_submit o=scan_X2Z_VA.txt d=scan_X2Z_VA n=90 submitScan.sub
+#condor_submit o=scan_X2Z_AV.txt d=scan_X2Z_AV n=90 submitScan.sub
+#condor_submit o=scan_X2Z_AA.txt d=scan_X2Z_AA n=90 submitScan.sub
+
+condor_submit o=scan_Xs2Z_SP.txt d=scan_Xs2Z_SP n=90 submitScan.sub
+condor_submit o=scan_Xs2Z_SA.txt d=scan_Xs2Z_SA n=90 submitScan.sub
+condor_submit o=scan_Xs2Z_VP.txt d=scan_Xs2Z_VP n=90 submitScan.sub
+condor_submit o=scan_Xs2Z_VV.txt d=scan_Xs2Z_VV n=90 submitScan.sub
+condor_submit o=scan_Xs2Z_VA.txt d=scan_Xs2Z_VA n=90 submitScan.sub
+condor_submit o=scan_Xs2Z_AV.txt d=scan_Xs2Z_AV n=90 submitScan.sub
+condor_submit o=scan_Xs2Z_AA.txt d=scan_Xs2Z_AA n=90 submitScan.sub
+
+condor_submit o=scan_Xs2Zs_SP.txt d=scan_Xs2Zs_SP n=90 submitScan.sub
+condor_submit o=scan_Xs2Zs_SA.txt d=scan_Xs2Zs_SA n=90 submitScan.sub
+condor_submit o=scan_Xs2Zs_VP.txt d=scan_Xs2Zs_VP n=90 submitScan.sub
+condor_submit o=scan_Xs2Zs_VV.txt d=scan_Xs2Zs_VV n=90 submitScan.sub
+condor_submit o=scan_Xs2Zs_VA.txt d=scan_Xs2Zs_VA n=90 submitScan.sub
+condor_submit o=scan_Xs2Zs_AV.txt d=scan_Xs2Zs_AV n=90 submitScan.sub
+condor_submit o=scan_Xs2Zs_AA.txt d=scan_Xs2Zs_AA n=90 submitScan.sub
+
